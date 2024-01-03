c23b96a4b3a38e5f0fde7fc87c5d2e5db6fc6962
dkt <1@dkt.com>
2024-01-03 11:10:15 +0800

    合并项目

--- 检测重合开发版.py
+++ 检测重合开发版.py
@@ -49,16 +49,16 @@ json_in = input("需要检测的文件：")
 
 # 获取形状列表
 for item in sorted(os.listdir(json_in)):
-    json_file: str = os.path.join(json_in, item)  # 文件的绝对路径
+    json_file: str = os.path.join(json_in, item)     # 文件的绝对路径
     with open(json_file, "r") as f:
-        json_dict = json.load(f)  # 转字典
+        json_dict = json.load(f)    # 转字典
         shapes = json_dict["shapes"]  # 获取所有形状的列表
         total_overlap = 0  # 用于累计重叠面积的总和
 
     # 遍历每个形状，与其他形状进行重叠面积计算
-    for i in range(len(shapes)):  # 遍历有几个形状
-        points1x = shapes[i]["points"][0][0]  # shapes[i]为当前的形状
-        points2x = shapes[i]["points"][1][0]  # [x][y]为形状左右两点的xy坐标
+    for i in range(len(shapes)):    # 遍历有几个形状
+        points1x = shapes[i]["points"][0][0]    # shapes[i]为当前的形状
+        points2x = shapes[i]["points"][1][0]    # [x][y]为形状左右两点的xy坐标
         points1y = shapes[i]["points"][0][1]
         points2y = shapes[i]["points"][1][1]
         for j in range(i + 1, len(shapes)):  # +1就是始终为当前形状的下一个 避免重复计算同一个形状对之间的重叠面积
